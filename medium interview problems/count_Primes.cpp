a85d425e6f56cac029b3b13e47100395be77c43d
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2022-07-26 19:45:55 +0530

    Create count_Primes.cpp

---
+++ medium interview problems/count_Primes.cpp
@@ -0,0 +1,71 @@
+//https://leetcode.com/problems/count-primes/
+
+//Below approach is not the best approach because if you run this time limit will exceed because here the constraint is 0 <= n <= 5 * 106.
+
+class Solution {
+private:
+    bool isPrime(int n){
+        if(n<=1)
+        return false;
+        
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    int countPrimes(int n) {
+        int cnt = 0;
+        for(int i=2; i<n; i++){
+            if(isPrime(i))
+            cnt++;
+        }
+        return cnt++;
+    }
+};
+
+class Solution {
+public:
+    int countPrimes(int n) {
+       int cnt = 0;
+        vector<bool> prime(n+1, true);
+        
+        prime[0] = prime[1] = false;
+        
+        for(int i=2; i<n; i++){
+            if(prime[i]){
+                cnt++;
+                for(int j=2*i; j<n; j=j+i){
+                    prime[j] = 0;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+//Here complexity is O(n^2)
+
+//So, we are solving it using Sieve of Eratosthenes
+
+class Solution {
+public:
+    int countPrimes(int n) {
+       int cnt = 0;
+        vector<bool> prime(n+1, true);
+        
+        prime[0] = prime[1] = false;
+        
+        for(int i=2; i<n; i++){
+            if(prime[i]){
+                cnt++;
+                for(int j=2*i; j<n; j=j+i){
+                    prime[j] = 0;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+//Here Time complexity is O(n*log(logn)
