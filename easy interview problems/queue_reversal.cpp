587d5620654100808b71c7e69f3095b87205fbd0
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2022-08-21 23:21:52 +0530

    Create queue_reversal.cpp

---
+++ easy interview problems/queue_reversal.cpp
@@ -0,0 +1,20 @@
+// https://practice.geeksforgeeks.org/problems/queue-reversal/1
+
+//Function to reverse the queue.
+queue<int> rev(queue<int> q)
+{
+    stack<int> s;
+    
+    while(!q.empty()){
+        int element = q.front();
+        q.pop();
+        s.push(element);
+    }
+    
+    while(!s.empty()){
+        int element = s.top();
+        s.pop();
+        q.push(element);
+    }
+    return q;
+}
