35557523b050961aed5c68433651473517ecf62e
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2022-09-01 21:39:33 +0530

    Update check_for_balanced_tree.cpp

--- easy interview problems/check_for_balanced_tree.cpp
+++ easy interview problems/check_for_balanced_tree.cpp
@@ -50,4 +50,40 @@ class Solution{
     
 };
 
-//Abovesolution is not optimized because here will get time complexity O(n^2) 
+//Above solution is not optimized because here will get time complexity O(n^2) 
+//so, use below approach 
+
+class Solution{
+    private:
+        int height(struct Node* node){
+            if(node == NULL){
+                return 0;
+            }
+            
+            int left = height(node -> left);
+            int right = height(node -> right);
+            
+            int ans = max(left, right) + 1;
+            return ans;
+        }
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    bool isBalanced(Node *root)
+    {
+        if(root == NULL){
+            return true;
+        }
+        bool left = isBalanced(root -> left);
+        bool right = isBalanced(root -> right);
+        
+        bool diff = abs(height(root -> left) - height(root -> right)) <= 1;
+        
+        if(left && right && diff){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+};
