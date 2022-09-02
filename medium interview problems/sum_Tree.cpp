9e6ae9508d4fec12e634a273e6e2d8deb35b4f6b
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2022-09-02 12:09:27 +0530

    Create sum_Tree.cpp

---
+++ medium interview problems/sum_Tree.cpp
@@ -0,0 +1,43 @@
+// https://practice.geeksforgeeks.org/problems/sum-tree/1
+
+class Solution
+{
+    public:
+    pair<bool, int> isSumTreeFast(Node* root){
+        if(root == NULL){
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        
+        if(root -> left == NULL && root -> right == NULL){
+            pair<bool, int> p = make_pair(true, root -> data);
+            return p;
+        }
+        
+        pair<bool, int> leftAns = isSumTreeFast(root -> left);
+        pair<bool, int> rightAns = isSumTreeFast(root -> right);
+        
+        bool isLeftSumTree = leftAns.first;
+        bool isRightSumTree = rightAns.first;
+        
+        int leftSum = leftAns.second;
+        int rightSum = rightAns.second;
+        
+        bool condn = root -> data == leftSum + rightSum;
+        
+        pair<bool, int> ans;
+        
+        if(isLeftSumTree && isRightSumTree && condn){
+            ans.first = true;
+            ans.second = root -> data + leftSum + rightSum;
+        }
+        else{
+            ans.first = false;
+        }
+        return ans;
+    }
+    bool isSumTree(Node* root)
+    {
+        return isSumTreeFast(root).first;    
+    }
+};
