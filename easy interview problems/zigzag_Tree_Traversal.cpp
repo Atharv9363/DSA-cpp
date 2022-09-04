b400e212e632940af24c115581f92e92bb7d2167
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2022-09-04 12:39:37 +0530

    Create zigzag_Tree_Traversal.cpp

---
+++ easy interview problems/zigzag_Tree_Traversal.cpp
@@ -0,0 +1,48 @@
+// https://practice.geeksforgeeks.org/problems/zigzag-tree-traversal/1
+
+class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	vector<int> result;
+    	if(root == NULL)
+    	    return result;
+    	
+    	queue<Node*> q;
+    	q.push(root);
+    	
+    	bool leftToRight = true;
+    	
+    	while(!q.empty()){
+    	    int size = q.size();
+    	    vector<int> ans(size);
+    	    
+    	    //Level process
+    	    for(int i=0; i<size; i++){
+    	        Node* frontNode = q.front();
+    	        q.pop();
+    	        
+    	        //normal insert or reverse insert
+    	        int index = leftToRight ? i : size - i - 1;
+    	        ans[index] = frontNode -> data;
+    	        
+    	        if(frontNode -> left)
+    	            q.push(frontNode -> left);
+    	        
+    	        if(frontNode -> right)
+    	            q.push(frontNode -> right);
+    	    }
+    	    //Changing direction
+    	    leftToRight = !leftToRight;
+    	    
+    	    for(auto i: ans){
+    	        result.push_back(i);
+    	    }
+    	}
+    	return result;
+    }
+};
+
+//Time Complexity : O(n)
+//Space Complexity : O(n)
