fcd73b6fe86193ed81970c8915fc3aeaa5a2f381
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2022-09-08 12:12:16 +0530

    Create k_sum_paths.cpp

---
+++ medium interview problems/k_sum_paths.cpp
@@ -0,0 +1,33 @@
+// https://practice.geeksforgeeks.org/problems/k-sum-paths/1
+
+class Solution{
+  public:
+    void solve(Node* root, int k, int &count, vector<int> path){
+        if(root == NULL){
+            return ;
+        }
+        path.push_back(root -> data);
+        
+        solve(root -> left, k, count, path);
+        
+        solve(root -> right, k, count, path);
+        
+        //check for k sum
+        int size = path.size();
+        int sum = 0;
+        for(int i = size-1; i>=0; i--){
+            sum += path[i];
+            if(sum == k){
+                count++;
+            }
+        }
+        path.pop_back();
+    }
+    int sumK(Node *root,int k)
+    {
+        vector<int> path;
+        int count = 0;
+        solve(root, k, count, path);
+        return count;
+    }
+};
