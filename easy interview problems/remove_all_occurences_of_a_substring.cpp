3dc0a36e515b5619e46fe3cd2aee62ee728361b7
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2023-08-14 22:51:11 +0530

    Update remove_all_occurences_of_a_substring.cpp

--- easy interview problems/remove_all_occurences_of_a_substring.cpp
+++ easy interview problems/remove_all_occurences_of_a_substring.cpp
@@ -7,4 +7,13 @@ public:
         }
         return s;
     }
+
+    // OR
+
+    int pos = s.find(part);
+        while(pos != string::npos){
+            s.erase(pos, part.length());
+            pos = s.find(part);
+        }
+        return s;
 };
