018d5bd811a7a623a78f38e18e6b2b313237469c
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2023-08-14 22:49:12 +0530

    Create Remove_All_Adjacent_Duplicates_In_String.cpp

---
+++ easy interview problems/Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        int i = 0;
+        
+        while(i < s.length()){
+            if(ans.length() > 0){
+                if(ans[ans.length() - 1] == s[i]){
+                    ans.pop_back();
+                }
+                else{
+                    ans.push_back(s[i]);
+                }
+            }
+            else{
+                ans.push_back(s[i]);
+            }
+            i++;
+        }
+        return ans;
+
+    }
+};
+
