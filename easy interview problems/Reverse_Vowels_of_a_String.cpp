1ee3a528556bfd1301f3a0d5ae12348e43e63fca
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2023-08-18 17:21:43 +0530

    Create Reverse_Vowels_of_a_String.cpp

---
+++ easy interview problems/Reverse_Vowels_of_a_String.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+
+class Solution {
+public:
+    bool isVowel(char ch){
+        ch = tolower(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+
+    string reverseVowels(string s) {
+        int l = 0, h = s.size() - 1;
+
+        while(l < h){
+            if(isVowel(s[l]) && isVowel(s[h])){
+                swap(s[l], s[h]);
+                l++;
+                h--;
+            }
+            else if(isVowel(s[l]) == 0){
+                l++;
+            }
+            else{
+                h--;
+            }
+        }
+        return s;
+    }
+};
