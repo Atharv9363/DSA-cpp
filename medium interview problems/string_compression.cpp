ab191f67203412e137a90b0bd42d60059d55bb52
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2023-09-02 17:55:47 +0530

    Update string_compression.cpp

--- medium interview problems/string_compression.cpp
+++ medium interview problems/string_compression.cpp
@@ -2,28 +2,69 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i = 0;
-        int ansIndex = 0;
-        int n = chars.size();
+        // int i = 0;
+        // int ansIndex = 0;
+        // int n = chars.size();
         
-        while(i<n){
-            int j=i+1;
-            while(j<n && chars[i]==chars[j]){
-                j++;
+        // while(i<n){
+        //     int j=i+1;
+        //     while(j<n && chars[i]==chars[j]){
+        //         j++;
+        //     }
+            
+        //     chars[ansIndex++] = chars[i];
+            
+        //     int count = j-i;
+            
+        //     if(count > 1){
+        //         string cnt = to_string(count);
+        //         for(char ch: cnt){
+        //             chars[ansIndex++] = ch;
+        //         }
+        //     }
+        //     i=j;
+        // }
+        // return ansIndex;
+    }
+};
+
+// New Solution :
+
+class Solution {
+public:
+    int compress(vector<char>& s) {
+        int index = 0, count = 1;
+        char prev = s[0];
+
+        for(int i = 1; i < s.size(); ++i){
+            if(s[i] == prev){
+                count++;
             }
-            
-            chars[ansIndex++] = chars[i];
-            
-            int count = j-i;
-            
-            if(count > 1){
-                string cnt = to_string(count);
-                for(char ch: cnt){
-                    chars[ansIndex++] = ch;
+            else{
+                s[index++] = prev;
+                if(count > 1){
+                    int start = index;
+                    while(count){
+                        s[index++] = (count % 10) + '0';
+                        count /= 10;
+                    }
+                    reverse(s.begin() + start, s.begin() + index);
                 }
+                prev = s[i];
+                count = 1; 
             }
-            i=j;
         }
-        return ansIndex;
+
+        s[index++] = prev;
+        if(count > 1){
+            int start = index;
+            while(count){
+                s[index++] = (count % 10) + '0';
+                count /= 10;
+            }
+            reverse(s.begin() + start, s.begin() + index);
+        }
+
+        return index;
     }
 };
