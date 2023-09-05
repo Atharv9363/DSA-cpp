c04bb94b1bb2a5d3cf31ef32d3889444f1917cab
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2023-09-05 15:19:03 +0530

    Update count_Primes.cpp

--- medium interview problems/count_Primes.cpp
+++ medium interview problems/count_Primes.cpp
@@ -68,4 +68,31 @@ public:
         return cnt;
     }
 };
+
+// OR
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n == 0) return 0;
+
+        vector<bool> prime(n, true);
+        prime[0] = prime[1] = false;
+
+        int ans = 0;
+
+        for(int i = 2; i < n; i++){
+            if(prime[i]){
+                ans++;
+
+                int j = 2 * i;
+                while(j < n){
+                    prime[j] = false;
+                    j += i; 
+                }
+            }
+        }
+        return ans;
+    }
+};
 //Here Time complexity is O(n*log(logn)
