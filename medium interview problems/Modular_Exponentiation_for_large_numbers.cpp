32dc023ea99576fa655876640df794a03d00d44e
Atharv9363 <79706892+Atharv9363@users.noreply.github.com>
2023-09-05 18:58:39 +0530

    Create Modular_Exponentiation_for_large_numbers.cpp

---
+++ medium interview problems/Modular_Exponentiation_for_large_numbers.cpp
@@ -0,0 +1,19 @@
+// https://practice.geeksforgeeks.org/problems/modular-exponentiation-for-large-numbers5537/1
+
+class Solution
+{
+	public:
+		long long int PowMod(long long int x,long long int n,long long int M)
+		{
+		    long long int ans = 1;
+		    
+		    while(n > 0){
+		        if(n&1){
+		            ans = (ans * x) % M;
+		        }
+		        x = (x * x) % M;
+		        n >>= 1;
+		    }
+		    return ans % M;
+		}
+};
